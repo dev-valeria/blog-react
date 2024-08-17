4c496f6f824f09c93c43f6132545b3b24f2bdbc8
Valeria <valleriademelo@gmail.com>
2024-08-17 19:09:11 -0300

    feat: add and configure NavBar and Footer components

--- miniblog/src/App.js
+++ miniblog/src/App.js
@@ -5,17 +5,21 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 //pages
 import Home from './pages/Home/home';
 import About from './pages/About/About';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className='app'>
       <BrowserRouter>
+        <Navbar />
         <div className='container'>
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/about' element={<About />} />
         </Routes>
         </div>
+        <Footer />
       </BrowserRouter>
     </div>
     
---
+++ miniblog/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+//CSS
+//import styles from "./Footer.module.css"
+
+
+const Footer = () => {
+  return (
+    <Footer>
+      <h3>Escreva sobre o que voce tem interesse!</h3>
+    <p>Mini Blog &copy; 2022</p>
+    </Footer>
+  );
+}
+
+export default Footer;
