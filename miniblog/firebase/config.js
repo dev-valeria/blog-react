8f8586e70c36d55b70e0554100ed99edd01d5f3e
Valeria <valleriademelo@gmail.com>
2024-08-17 17:57:00 -0300

    feat: initialize new project and integrate Firebase with React

---
+++ miniblog/firebase/config.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firebase"
+const firebaseConfig = {
+  apiKey: "AIzaSyA7WJF8SjZxSurMrf4XJo4smBcgzLLY6wI",
+  authDomain: "miniblog-8f114.firebaseapp.com",
+  projectId: "miniblog-8f114",
+  storageBucket: "miniblog-8f114.appspot.com",
+  messagingSenderId: "374413084990",
+  appId: "1:374413084990:web:73418ee74b696247b0846f",
+  measurementId: "G-VKT20QCJZL"
+};
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const db = getFirestore(app);
+
+export { db };
+export { analytics };
