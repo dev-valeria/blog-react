c192390c6d8ab34af6960e0ee4bfdc2b4b38a199
Valeria <valleriademelo@gmail.com>
2024-08-19 22:56:25 -0300

    feat: Implement About page with HTML and CSS, and restrict access to authentication-required pages

--- miniblog/src/pages/About/About.js
+++ miniblog/src/pages/About/About.js
@@ -1,11 +1,15 @@
 //CSS
 import styles from "./About.module.css"
+import { Link } from "react-router-dom"
 
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-
+    <div className={styles.about}>
+      <h2>Sobre o Mini<span>Blog</span></h2>
+      <p>Este projeto consiste em um blog feito com React no Front-End e Firebase no Back-End.</p>
+      <p>
+        <Link to="/posts/create" className="btn">Criar Post</Link> 
+      </p>
     </div>
   )
 }
